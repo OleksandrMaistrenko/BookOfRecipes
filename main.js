092b86f6bf88ae34323e78c5204b8f28b54c3c8c
Oleksandr Maistrenko <oleksandrmaistrenko100@gmail.com>
2024-02-05 18:34:21 +0100

    const fix

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 import { Recipe } from "./recipe.js";
 import { BookOfRecipes } from "./recipes.js";
 
-let recipe1 = new Recipe(
+const recipe1 = new Recipe(
   "Dish 1",
   ["Ingredient1", "Ingredient2"],
   "Description",
   30
 );
-let recipe2 = new Recipe(
+const recipe2 = new Recipe(
   "Dish 2",
   ["Ingredient1", "Ingredient3"],
   "Description",
   60
 );
-let recipe3 = new Recipe("Dish 3", ["Potato", "Carrot"], "Description", 120);
-let recipe4 = new Recipe("Dish 4", ["Potato"], "Description", 45);
-let recipe5 = new Recipe("Invalid Dish", [], "", -10);
+const recipe3 = new Recipe("Dish 3", ["Potato", "Carrot"], "Description", 120);
+const recipe4 = new Recipe("Dish 4", ["Potato"], "Description", 45);
+const recipe5 = new Recipe("Invalid Dish", [], "", -10);
 
 const bookOfRecipes = new BookOfRecipes();
 
@@ -25,13 +25,13 @@ bookOfRecipes.addRecipe(recipe3);
 bookOfRecipes.addRecipe(recipe4);
 bookOfRecipes.addRecipe(recipe5);
 
-let recipesByTime = bookOfRecipes.findRecipeByTime(60);
+const recipesByTime = bookOfRecipes.findRecipeByTime(60);
 console.log(
   "Recipes by time:",
   recipesByTime.map((recipe) => recipe.name).join(", ")
 );
 
-let recipesByIngredients = bookOfRecipes.findRecipeByIngredients([
+const recipesByIngredients = bookOfRecipes.findRecipeByIngredients([
   "Potato",
   "Carrot",
 ]);
